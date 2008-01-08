96ba0803d8531ab8a37d789ce5b95741a766d622
colding@omesc.com <colding@omesc.com@ba4e19d9-35d9-4a09-b31a-9dc37018f685>
2008-01-08 12:42:32 +0000

    Added Linux build script
    git-svn-id: http://svn.42tools.net/repos/lorica/trunk@42 ba4e19d9-35d9-4a09-b31a-9dc37018f685

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+WGET=/usr/bin/wget
+TAR=/bin/tar
+ACETAO=ACE+TAO-5.6.2.tar.bz2
+
+export LORICA_ROOT="$(pwd)"
+export ACE_ROOT="$LORICA_ROOT/ACE_wrappers"
+export TAO_ROOT="$ACE_ROOT/TAO"
+
+export LD_LIBRARY_PATH="$LORICA_ROOT/lib:$ACE_ROOT/lib:$LD_LIBRARY_PATH"	
+export PATH="$LORICA_ROOT/bin:$ACE_ROOT/bin:$PATH"
+
+
+#
+# build TAO
+#
+
+# get the source
+cd $LORICA_ROOT
+$WGET -nc http://download.dre.vanderbilt.edu/previous_versions/$ACETAO
+if [ ! -d "$TAO_ROOT" ]; then
+    $TAR xjf $ACETAO
+fi
+
+# prepare for build
+echo '#include "ace/config-linux.h"' > $ACE_ROOT/ace/config.h
+echo 'include $(ACE_ROOT)/include/makeinclude/platform_linux.GNU' > $ACE_ROOT/include/makeinclude/platform_macros.GNU
+
+# just build the ACE library
+echo ""
+echo " *******************************************************"
+echo " *                 Building ACE                        *" 
+echo " *******************************************************"
+echo ""
+cd $ACE_ROOT/ace
+make
+
+# used by TAO
+echo ""
+echo " *******************************************************"
+echo " *                Building gperf                       *" 
+echo " *******************************************************"
+echo ""
+cd $ACE_ROOT/apps/gperf/src
+make
+
+echo ""
+echo " *******************************************************"
+echo " *                 Building TAO                        *" 
+echo " *******************************************************"
+echo ""
+cd $ACE_ROOT/TAO
+make
+
+
+#
+# build Lorica
+#
+
+echo ""
+echo " *******************************************************"
+echo " *                Building Lorica                      *" 
+echo " *******************************************************"
+echo ""
+cd $LORICA_ROOT
+$ACE_ROOT/bin/mwc.pl -type gnuace lorica.mwc
+make
+
