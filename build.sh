275d63277993d63f5b7e8726e87c4a4e84ecfd59
colding@omesc.com <colding@omesc.com@ba4e19d9-35d9-4a09-b31a-9dc37018f685>
2008-01-09 10:56:21 +0000

    Detect wget or curl in build script
    git-svn-id: http://svn.42tools.net/repos/lorica/trunk@44 ba4e19d9-35d9-4a09-b31a-9dc37018f685

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-WGET=/usr/bin/wget
-TAR=/bin/tar
 ACETAO=ACE+TAO-5.6.2.tar.bz2
 
 export LORICA_ROOT="$(pwd)"
@@ -11,16 +9,53 @@ export TAO_ROOT="$ACE_ROOT/TAO"
 export LD_LIBRARY_PATH="$LORICA_ROOT/lib:$ACE_ROOT/lib:$LD_LIBRARY_PATH"	
 export PATH="$LORICA_ROOT/bin:$ACE_ROOT/bin:$PATH"
 
+cmd=$(which tar)
+if [ 0 -eq $? ]; then
+    TAR=$(which tar)
+else
+    echo "You need tar to run this script"
+    exit 1
+fi
 
-#
-# build TAO
-#
-
-# get the source
 cd $LORICA_ROOT
-$WGET -nc http://download.dre.vanderbilt.edu/previous_versions/$ACETAO
+
+#
+# get and build TAO
+#
+
+# determine download command and execute it
+DOWNLOAD=""
+if [ ! -f "$ACETAO" ]; then
+    cmd=$(which wget)
+    if [ 0 -eq $? ]; then
+	$(which wget) -nc http://download.dre.vanderbilt.edu/previous_versions/$ACETAO
+	    if [ 0 -eq $? ]; then
+		DOWNLOAD=OK
+	    fi
+    fi
+    if [ -z "$DOWNLOAD" ]; then
+	cmd=$(which curl)
+	if [ 0 -eq $? ]; then
+	    $(which curl) http://download.dre.vanderbilt.edu/previous_versions/$ACETAO > $ACETAO
+	    if [ 0 -eq $? ]; then
+		DOWNLOAD=OK
+	    fi
+	fi
+    fi
+    if [ -z "$DOWNLOAD" ]; then
+	echo "You need a working internet connection and curl(1) or wget(1) to run this script"
+	exit 1
+    fi
+
+fi
+
+# extract TAO
 if [ ! -d "$TAO_ROOT" ]; then
     $TAR xjf $ACETAO
+fi
+if [ 0 -eq $? ]; then
+    echo "\'tar xjf\' didn't work - bzip2 not supported?"
+    exit 1
 fi
 
 # prepare for build
